14ae591642d927a92c360f600acac8794a773e0f
12singhmar <12singhmar@gmail.com>
2026-06-07 23:29:26 +0530

    string files manipulation

--- basics/9-string-method..py
+++ basics/9-string-method..py
@@ -14,3 +14,26 @@ phone = "123-456-789"
 #print(result)
 
 print(help(str))
+
+
+#----------------------------------------------------------------------------
+
+#indexing
+#credit card number = ccno
+
+ccno = "1234 5678"
+
+print(ccno[0])
+
+#the way it works is start:end(not included):step
+# so 1:5:2 states start from 1 to 4 in steps of 2
+print(ccno[1:5:2])
+
+# :5 means from start to 4
+# 0: means from 0 to end
+# ::2 means from start to end in steps of 2
+
+#reversing-
+print(ccno[::-1])
+
+#-x means xth from the end
